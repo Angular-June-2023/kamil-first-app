149c603688dc179ce80d206e3e59325c2077f4a2
Kamil Tomanek <ktoman@softserveinc.com>
2023-06-14 13:16:08 +0200

    Add navigation buttons (router and lazy loading)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cv', loadChildren: () => import('./cv/cv.module').then(m => m.CvModule) }, 
+  { path: 'about-me', loadChildren: () => import('./about-me/about-me.module').then(m => m.AboutMeModule) },
+  { path: '', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
